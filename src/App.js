32a45306002c1491eab80b6b2d13ddfa1cfd3802
Robert Åhlund <robert.ahlund@gmail.com>
2017-09-30 16:04:32 +0200

    skrev om urlchecken, conditional rendering via props, radering av fordon ligger nu i redigeringskomponenten.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import AllVehicles from "./AllVehicles";
 import CancelBooking from "./CancelBooking";
 import AddVehicle from "./AddVehicle";
-import RemoveVehicle from "./RemoveVehicle";
 import EditVehicle from "./EditVehicle";
 import Menu from "./Menu";
 import BookVehicle from "./BookVehicle";
@@ -16,20 +15,8 @@ class App extends Component {
       admin: true,
       location: '',
       bookVehicle: false,
-      vehicleData: {
-        type: "personbil",
-        license: "B",
-        brand: "Mazda",
-        model: "323i",
-        year: 1991,
-        gearbox: "manuell",
-        price: 298,
-        image: "https://linktofile.jpg",
-        bookable: true,
-        bookDates: [],
-        notes: 'String'
-      },
-      database: ''
+      vehicleData: [],
+      database: []
     }
   }
 
@@ -38,46 +25,17 @@ class App extends Component {
     window.addEventListener("hashchange", () => {
       this.checkUrl()
     });
+
     this.setState({
       database: placeholderData
     })
   }
 
   checkUrl() {
-    switch (window.location.hash) {
-      case '#add':
-        this.setState({
-          location: 'add'
-        });
-        break;
-      case '#show':
-        this.setState({
-          location: 'show'
-        });
-        break;
-      case '#book':
-        this.setState({
-          location: 'book'
-        });
-        break;
-      case '#cancel':
-        this.setState({
-          location: 'cancel'
-        });
-        break;
-      case '#remove':
-        this.setState({
-          location: 'remove'
-        });
-        break;
-      case '#edit':
-        this.setState({
-          location: 'edit'
-        });
-        break;
-      default:
-        break;
-    }
+    const currentLocation = window.location.hash.replace('#', '');
+    this.setState({
+      location: currentLocation
+    })
   }
 
   handleLogin() {
@@ -89,14 +47,14 @@ class App extends Component {
   vehicleBooking(event) {
     //för att se till att oavsett vad vi klickar på i listan så är event.target alltid ett li element
     let target = event.target;
-    if (event.target.localName === 'span' || event.target.localName === 'div') {
-      target = event.target.parentElement;
-    } else if (event.target.localName === 'img') {
-      target = event.target.parentElement.parentElement;
+    if (target.localName === 'span' || target.localName === 'div') {
+      target = target.parentElement;
+    } else if (target.localName === 'img') {
+      target = target.parentElement.parentElement;
     }
 
-    const model = target.children[2].innerText.toLowerCase();
-    const findCarInDatabase = this.state.database.filter(vehicle => vehicle.model.toLowerCase().indexOf(model) > -1);
+    const carModel = target.children[2].innerText.toLowerCase();
+    const findCarInDatabase = this.state.database.filter(vehicle => vehicle.model.toLowerCase().indexOf(carModel) > -1);
     console.log(findCarInDatabase);
     this.setState({
       vehicleData: findCarInDatabase,
@@ -105,7 +63,8 @@ class App extends Component {
   };
 
   handleModal(event) {
-    if (event.target.localName === 'section' || event.target.innerText === 'Stäng') {
+    const target = event.target;
+    if (target.localName === 'section' || target.innerText === 'Stäng') {
       this.setState({
         bookVehicle: !this.state.bookVehicle
       })
@@ -120,44 +79,28 @@ class App extends Component {
           checkUrl={this.checkUrl.bind(this)}
           handleLogin={this.handleLogin.bind(this)}
         />
-        {this.state.location === 'show' &&
         <AllVehicles
+          if={this.state.location === 'show'}
           data={this.state.database}
           vehicleBooking={this.vehicleBooking.bind(this)}
         />
-        }
-        {this.state.location === 'cancel' &&
-        <CancelBooking/>
-        }
-        {this.state.admin &&
-        <div>
-          {this.state.location === 'add' &&
-          <AddVehicle/>
-          }
-          {this.state.location === 'remove' &&
-          <RemoveVehicle/>
-          }
-          {this.state.location === 'edit' &&
-          <EditVehicle/>
-          }
-        </div>
-        }
-        {this.state.bookVehicle &&
+        <CancelBooking
+          if={this.state.location === 'cancel'}
+        />
+        <AddVehicle
+          if={this.state.location === 'add' && this.state.admin}
+        />
+        <EditVehicle
+          if={this.state.location === 'edit' && this.state.admin}
+        />
         <BookVehicle
+          if={this.state.bookVehicle === true}
           data={this.state.vehicleData}
           closeModal={this.handleModal.bind(this)}
         />
-        }
       </div>
     );
   }
 }
-
-/*
-  data.forEach(el => {
-    var x = "gearbox" in el ? el.gearbox : 'unknown'
-    var x = el.gearbox || 'Unknown'
-})
- */
 
 export default App;
