9bad13e40e093d70a74e6efdbb04dae3ce702aac
Mille <IamMille@users.noreply.github.com>
2017-10-04 21:07:18 +0200

    added working calender, and booking

--- src/components/BookVehicle.js
+++ src/components/BookVehicle.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import DayPicker, {DateUtils} from 'react-day-picker';
+import config from '../config.json';
 
 import 'react-day-picker/lib/style.css';
 
@@ -9,31 +10,92 @@ export default class BookVehicle extends Component
 {
     state = {
         selectedDays: [],
+        disabledDays: []
     };
 
-    handleDayClick = (day, { selected }) => {
-        const { selectedDays } = this.state;
+    componentDidMount() {
+        console.log("bookVehicle did mount");
 
-        if (selected) { // deselect
+        fetch(config.apiRoot + "booking/read") // TODO: SEARCH FOR THIS CAR, NOT HOLE DB
+            .then(resp => resp.json())
+            .then(json => {
+                let disabledDays = [];
+
+                json.data.forEach(booking => {
+                    disabledDays.push({
+                        after: new Date(booking.dateFrom).addDays(-1),
+                        before: new Date(booking.dateTill).addDays(1)
+                    })
+                });
+
+                this.setState({ disabledDays })
+            })
+            .catch(error => {
+                console.warn("API error:", error);
+            });
+        console.log("bookVehicle did mount");
+
+    }
+
+    handleDayClick = (day, { selected, disabled }) =>
+    {
+        const {selectedDays, disabledDays} = this.state;
+
+        console.log("handleDayClick:", disabledDays);
+
+        if (selected) {
             const selectedIndex = selectedDays.findIndex(selectedDay =>
                 DateUtils.isSameDay(selectedDay, day)
             );
             selectedDays.splice(selectedIndex, 1);
-
-        } else {
-            // /select
-            selectedDays.push(day);
         }
-        this.setState({ selectedDays });
+        else {
+            // if disabled, DONT PUSH
+            console.log("push date");
+            if (selectedDays.length === 0 || this.isCohesiveDate(day))
+                selectedDays.push(day);
+        }
+
+        this.setState({selectedDays});
     };
 
     isCohesiveDate = (day) => {
-        return Boolean(this.selectedDays.filter(d =>
-            DateUtils.isDayAfter(day, d) || DateUtils.isDayBefore(day, d)
-        ));
+        let dayAfter  = day.addDays(1);
+        let dayBefore = day.addDays(-1);
+
+        return Boolean(this.state.selectedDays.filter(d =>
+            DateUtils.isSameDay(d, dayAfter) || DateUtils.isSameDay(d, dayBefore)
+        ).length);
+    };
+
+    serializeUpdateObject = (obj) => {
+        return Object.keys(obj).map(prop => {
+            if (prop === '_id') return false;
+            else return encodeURIComponent(prop) + '=' + encodeURIComponent(obj[prop])
+        }).join('&')
     };
 
     handleSubmit = () => {
+        const {selectedDays} = this.state;
+
+        let vehicleId = this.props.data[0]._id;
+        let dateTill = new Date(Math.max.apply(null,selectedDays));
+        let dateFrom = new Date(Math.min.apply(null,selectedDays));
+        let createBooking = {vehicleId, dateFrom, dateTill};
+        console.log(this.serializeUpdateObject(createBooking))
+
+        fetch( config.apiRoot + "booking/create/?" + this.serializeUpdateObject(createBooking) )
+            .then(resp => resp.json())
+            .then(json => {
+                // show success message to the user
+                console.log("API response:", json);
+            })
+            .catch(error => {
+                // show error message to the user (validation is handles by the api/model)
+                console.error("API error:", error);
+            });
+        // close modal
+        //this.props.setState({editVehicle: false});
 
     };
 
@@ -42,12 +104,12 @@ export default class BookVehicle extends Component
         return <section className="book-vehicle" onClick={this.props.closeBookModal}>
 
             <div className="book-vehicle-container">
-                {console.log(this.props.data)}
 
                 <div className="image-container-book">
                     {/*<img src={this.props.data[0].image || "http://via.placeholder.com/150x150"} alt="bild"/>*/}
                     <DayPicker
                         selectedDays={this.state.selectedDays}
+                        disabledDays={this.state.disabledDays}
                         onDayClick={this.handleDayClick}
                     />
                 </div>
@@ -65,13 +127,19 @@ export default class BookVehicle extends Component
                     </ul>
                 </div>
                 <div className="button-container">
-                    <button className="button" onClick={this.handelSubmit}>Boka</button>
+                    <button className="button" onClick={this.handleSubmit}>Boka</button>
                     <button className="button" onClick={this.props.closeBookModal}>Stäng</button>
                 </div>
             </div>
         </section>;
     }
 }
+
+Date.prototype.addDays = function(days) {
+    let date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+};
 
 BookVehicle.propTypes = {
     data: PropTypes.array.isRequired,
